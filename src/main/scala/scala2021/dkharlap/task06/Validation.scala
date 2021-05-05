838f5ce9c46e7eda4a41f6949cafdc0e2830f6b1
Dzianis Kharlap <dkharlap@exadel.com>
2021-05-05 21:51:19 +0300

    Task06 implementation

--- src/main/scala/scala2021/dkharlap/task06/Validation.scala
+++ src/main/scala/scala2021/dkharlap/task06/Validation.scala
@@ -1,6 +1,6 @@
 package scala2021.dkharlap.task06
 
-import scala2021.dkharlap.task06.Sex.{FEMALE, MALE, Sex}
+import scala2021.dkharlap.task06.Sex.{FEMALE, MALE}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.DurationInt
@@ -22,93 +22,96 @@ object Validation {
       Person("Chad", 34, "chad@yandex.ru", MALE, 56.1),
     )
     println("Return validation results by validateAndGetFirst()")
-    people.foreach(person => println(validateAndGetFirst(person)))
+    people.foreach(person => println("Validate object " + person + ": " + printValidationResultToConsole(List(validateAndGetFirst(person)))))
     println("")
     println("Return validation results by validateAndGetAll()")
-    people.foreach(person => println(validateAndGetAll(person)))
+    people.foreach(person => println("Validate object " + person + ": " + printValidationResultToConsole(validateAndGetAll(person))))
     println("")
     println("Return validation results by validateAndGetAllInParallel()")
-    people.foreach(person => println(validateAndGetAllInParallel(person)))
+    people.foreach(person => println("Validate object " + person + ": " + printValidationResultToConsole(validateAndGetAllInParallel(person))))
   }
 
-  def validateAndGetFirst(person: Person): String = {
-    val validation = validateName(person.name) match {
-      case nameValidation if nameValidation.isLeft => nameValidation.left.getOrElse("Name Validation - Unknown error")
-      case nameValidation if nameValidation.isRight =>
-        validateAge(person.age) match {
-          case ageValidation if ageValidation.isLeft => ageValidation.left.getOrElse("Age Validation - Unknown error")
-          case ageValidation if ageValidation.isRight =>
-            validateEmail(person.email) match {
-              case emailValidation if emailValidation.isLeft => emailValidation.left.getOrElse("Email Validation - Unknown error")
-              case emailValidation if emailValidation.isRight =>
-                validateSexAndHeight(person.sex, person.height) match {
-                  case complexValidation if complexValidation.isLeft => complexValidation.left.getOrElse("Complex Validation (Sex and Height) - Unknown error")
-                  case complexValidation if complexValidation.isRight => "Validation is successful"
-                }
-            }
-        }
-    }
-    "Validate object " + person + ": " + validation
+  def validateAndGetFirst(person: Person): Either[String, Person] = {
+    for {
+      _ <- validateName(person)
+      _ <- validateAge(person)
+      _ <- validateEmail(person)
+      result <- validateSexAndHeight(person)
+    } yield result
   }
 
-  def validateAndGetAll(person: Person): String = {
-    val nameValidation = validateName(person.name)
-    val ageValidation = validateAge(person.age)
-    val emailValidation = validateEmail(person.email)
-    val complexValidation = validateSexAndHeight(person.sex, person.height)
-    val errors = List(nameValidation, ageValidation, emailValidation, complexValidation).filter(_.isLeft)
-    "Validate object " + person + ": ".concat(if (errors.isEmpty) "Validation is successful" else errors.map(_.left.getOrElse(" Unknown error")).toString())
+  def validateAndGetAll(person: Person): List[Either[String, Person]] = {
+    val nameValidation = validateName(person)
+    val ageValidation = validateAge(person)
+    val emailValidation = validateEmail(person)
+    val complexValidation = validateSexAndHeight(person)
+    List(nameValidation, ageValidation, emailValidation, complexValidation)
   }
 
-  def validateAndGetAllInParallel(person: Person): String = {
-    val f1 = Future(validateName(person.name))
-    val f2 = Future(validateAge(person.age))
-    val f3 = Future(validateEmail(person.email))
-    val f4 = Future(validateSexAndHeight(person.sex, person.height))
+  def validateAndGetAllInParallel(person: Person): List[Either[String, Person]] = {
+    val f1 = Future(validateName(person))
+    val f2 = Future(validateAge(person))
+    val f3 = Future(validateEmail(person))
+    val f4 = Future(validateSexAndHeight(person))
 
     val validation = for {
       f1Result <- f1
       f2Result <- f2
       f3Result <- f3
       f4Result <- f4
-    } yield "Validate object " + person + ": ".concat(if (!List(f1Result, f2Result, f3Result, f4Result).exists(_.isLeft)) "Validation is successful"
-          else List(f1Result, f2Result, f3Result, f4Result).filter(_.isLeft).map(_.left.getOrElse(" Unknown error")).toString())
+    } yield List(f1Result, f2Result, f3Result, f4Result)
 
     Await.result(validation, 2.second)
   }
 
-  def validateName(name: String): Either[String, String] = {
-    name match {
-      case value if value.isEmpty => Left("Name should not be empty")
-      case value if !value.matches("[A-Za-z]+") => Left("Name should contain only latin letters")
-      case _ => Right(name)
+  def validateName(person: Person): Either[String, Person] = {
+    if (person.name.isEmpty) {
+      Left("Name should not be empty")
+    } else if (!person.name.matches("[A-Za-z]+")) {
+      Left("Name should contain only latin letters")
+    } else {
+      Right(person)
     }
   }
 
-  def validateAge(age: Int): Either[String, Int] = {
-    age match {
-      case value if (value <= 0 || value >= 100) => Left("Age should be greater than 0 and less than 100")
-      case _ => Right(age)
+  def validateAge(person: Person): Either[String, Person] = {
+    if (person.age <= 0 || person.age >= 100) {
+      Left("Age should be greater than 0 and less than 100")
+    } else {
+      Right(person)
     }
   }
 
-  def validateEmail(email: String): Either[String, String] = {
-    email match {
-      case value if value.isEmpty => Right(email)
-      case value if !value.matches("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$") => Left("Email is not valid")
-      case _ => Right(email)
+  def validateEmail(person: Person): Either[String, Person] = {
+    if (person.email.nonEmpty && !person.email.matches("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")) {
+      Left("Email is not valid")
+    } else {
+      Right(person)
     }
   }
 
-  def validateSexAndHeight(sex: Sex, height: Double): Either[String, (Sex, Double)] = {
-    if (sex == MALE) {
-      if (height > 100) {
-        Right((sex, height))
+  def validateSexAndHeight(person: Person): Either[String, Person] = {
+    if (person.sex == MALE) {
+      if (person.height > 100) {
+        Right(person)
       } else {
         Left("If sex is Male, then height should be greater than 100")
       }
     } else {
-      Right((sex, height))
+      Right(person)
+    }
+  }
+
+  def printValidationResultToConsole(result: List[Either[String, Person]]): String = {
+    if (result.nonEmpty) {
+      val errors = result.filter(_.isLeft)
+      if (errors.isEmpty) {
+        "Validation is successful"
+      } else {
+        errors.map(_.left.getOrElse(" Unknown error")).toString()
+      }
+    } else {
+      "Validation is successful"
     }
   }
 
